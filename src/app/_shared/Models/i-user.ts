2daf5eff4b7e58d2f036afdecf96286b31ddc6df
yemza <elbouazzaoui729@gmail.com>
2023-01-24 18:05:32 +0100

    adding models t

---
+++ src/app/_shared/Models/i-user.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+  id: number;
+  firstName: String;
+  lastName: String;
+  email: String;
+  userName: String;
+  BirthDate: String;
+  City: String;
+  Country: String;
+}
